71fb4e40a28d978b474f9e11d3cb52f6166e2dfb
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-11 20:52:35 -0300

    feat:adiciona entitys

---
+++ src/main/java/com/projeto/sistema_epi/entity/ColaboradorEntity.java
@@ -0,0 +1,36 @@
+package com.projeto.sistema_epi.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "departamento")
+public class ColaboradorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_colaborador")
+    private Long idColaborador;
+
+    @Column (name = "nome")
+    private String nome;
+
+    @Column (name = "cpf")
+    private String cpf;
+
+    @Column (name = "cargo")
+    private String cargo;
+
+    @Column (name = "setor")
+    private String setor;
+
+    @Column(name = "data_admissao")
+    private LocalDate dataAdmissao;
+
+    @Column(name = "status_ativo")
+    private boolean statusAtivo;
+
+
+
+}
---
+++ src/main/java/com/projeto/sistema_epi/entity/EpiEntity.java
@@ -0,0 +1,6 @@
+package com.projeto.sistema_epi.entity;
+
+public class EpiEntity {
+
+
+}
