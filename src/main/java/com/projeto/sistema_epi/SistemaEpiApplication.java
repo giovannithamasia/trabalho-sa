f756eac10d289ba20454ecf87945c00a791d15df
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-11 19:34:55 -0300

    init backend

---
+++ src/main/java/com/projeto/sistema_epi/SistemaEpiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.sistema_epi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/sistema_epi/SistemaEpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.sistema_epi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
