75d86428b846d0973bff9bf875d58dc8148dabc1
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-11 19:50:18 -0300

    teste

--- src/main/java/com/projeto/sistema_epi/SistemaEpiApplication.java
+++ src/main/java/com/projeto/sistema_epi/SistemaEpiApplication.java
@@ -8,6 +8,7 @@ public class SistemaEpiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaEpiApplication.class, args);
+		System.out.println("Teste");
 	}
 
 }
