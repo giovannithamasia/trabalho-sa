ae5581102a9cf62a3887d6e8f8cee30474802c3f
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2025-11-11 21:05:43 -0300

    feat:adicionar epientity

--- src/main/java/com/projeto/sistema_epi/entity/EpiEntity.java
+++ src/main/java/com/projeto/sistema_epi/entity/EpiEntity.java
@@ -1,16 +1,43 @@
 package com.projeto.sistema_epi.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity
+@Table(name = "epi")
 
 
 public class EpiEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_epi")
+    private Long idEpi;
 
+    @Column(name = "nome_epi")
+    private String nomeEpi;
+
+    @Column(name = "tipo_epi")
+    private String tipoEpi;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "tamanho")
+    private String tamanho;
+
+    @Column(name = "validade")
+    private LocalDate validade;
+
+    @Column(name = "situacao")
+    private String situacao;
 }
+
+
