cc32363476acc37c831fdca7cbac65201bb17552
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2025-11-11 21:11:35 -0300

    feat:adicionar emprestimoentity

---
+++ src/main/java/com/projeto/sistema_epi/entity/EmprestimoEntity.java
@@ -0,0 +1,44 @@
+package com.projeto.sistema_epi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class EmprestimoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_emprestimo")
+    private Long idEmprestimo;
+
+    @ManyToOne
+    @JoinColumn (name = "id_colaborador",referencedColumnName = "id_colaborador")
+    private ColaboradorEntity colaborador;
+
+    @ManyToOne
+    @JoinColumn (name = "id_epi",referencedColumnName = "id_epi")
+    private EpiEntity epi;
+
+    @Column(name = "data_emprestimo")
+    private LocalDateTime dataEmprestimo;
+
+    @Column (name = "data_prevista_devolucao")
+    private LocalDate dataPrevistaDevolucao;
+
+    @Column (name = "data_devolucao")
+    private LocalDateTime dataDevolucao;
+
+    @Column (name = "status")
+    private String status;
+
+    @Column (name = "observacao")
+    private String observacao;
+
+}
