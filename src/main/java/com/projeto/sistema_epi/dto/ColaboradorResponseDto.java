a631dfbfc36837abdd268aab4d888f97cabda778
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-13 20:17:49 -0300

    fix/movendo-responseDto

--- src/main/java/com/projeto/sistema_epi/dto/ColaboradorResponseDto.java
+++ src/main/java/com/projeto/sistema_epi/dto/ColaboradorResponseDto.java
@@ -1,9 +1,6 @@
-package dto;
+package com.projeto.sistema_epi.dto;
 
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.*;
 
 import java.util.Date;
