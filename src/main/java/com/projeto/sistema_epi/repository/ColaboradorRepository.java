4dcf48546e0b993ed1a74209c074ee335144f3c0
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-12 23:09:38 -0300

    feat: cria repositorys

---
+++ src/main/java/com/projeto/sistema_epi/repository/ColaboradorRepository.java
@@ -0,0 +1,7 @@
+package com.projeto.sistema_epi.repository;
+
+import com.projeto.sistema_epi.entity.ColaboradorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColaboradorRepository extends JpaRepository<ColaboradorEntity,Long> {
+}
