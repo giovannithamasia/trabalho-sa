f0154a937e66a84f715f42345b9a22e0b74cf1e4
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2025-11-13 21:23:06 -0300

    fix/removendo-restantes

--- src/main/java/com/projeto/sistema_epi/repository/EmprestimoRepository.java
+++ src/main/java/com/projeto/sistema_epi/repository/EmprestimoRepository.java
@@ -3,5 +3,5 @@ package com.projeto.sistema_epi.repository;
 import com.projeto.sistema_epi.entity.EmprestimoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity,Long> {
+public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity,Integer> {
 }
--- src/main/java/com/projeto/sistema_epi/repository/EpiRepository.java
+++ src/main/java/com/projeto/sistema_epi/repository/EpiRepository.java
@@ -3,5 +3,5 @@ package com.projeto.sistema_epi.repository;
 import com.projeto.sistema_epi.entity.EpiEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EpiRepository extends JpaRepository<EpiEntity,Long> {
+public interface EpiRepository extends JpaRepository<EpiEntity,Integer> {
 }
