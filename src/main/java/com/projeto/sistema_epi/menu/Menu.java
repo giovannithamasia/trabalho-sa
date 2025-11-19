97dc4b037d73bdf6d69fb642bef39a72a94f1023
Giovanni Thamasia <giovannithamasia2@gmail.com>
2025-11-18 21:32:20 -0300

    feat:adiciona menu no controller

---
+++ src/main/java/com/projeto/sistema_epi/menu/Menu.java
@@ -0,0 +1,20 @@
+package com.projeto.sistema_epi.menu;
+
+public class Menu {
+
+    public void imprimirMenu(){
+        System.out.println("1 - Cadastrar colaborador");
+        System.out.println("2 - Listar Colaboradores");
+        System.out.println("3 - Atualizar colaborador");
+        System.out.println("4 - Deletar Colaborador");
+        System.out.println("5 - Cadastrar EPI");
+        System.out.println("6 - Listar EPIs");
+        System.out.println("7 - Atualizar EPI");
+        System.out.println("8 - Deletar EPI");
+        System.out.println("9 - Retirar EPI");
+        System.out.println("10 - Devolver EPI");
+        System.out.println("11 - Listar Emprestimos");
+        System.out.println("12 - Sair");
+        System.out.println("Escolha uma opção: ");
+    }
+}
