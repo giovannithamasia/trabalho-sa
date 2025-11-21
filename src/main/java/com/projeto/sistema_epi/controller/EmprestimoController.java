ee6e26c449c09e81ab20763dd843fd790b762300
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-21 16:12:08 -0300

    feat: adiciona validação de existência de colaborador e EPI no EmprestimoController

--- src/main/java/com/projeto/sistema_epi/controller/EmprestimoController.java
+++ src/main/java/com/projeto/sistema_epi/controller/EmprestimoController.java
@@ -1,12 +1,17 @@
 package com.projeto.sistema_epi.controller;
 
 import com.projeto.sistema_epi.dto.EmprestimoDto;
+import com.projeto.sistema_epi.dto.EmprestimoResponseDto;
+import com.projeto.sistema_epi.repository.ColaboradorRepository;
+import com.projeto.sistema_epi.repository.EmprestimoRepository;
+import com.projeto.sistema_epi.repository.EpiRepository;
 import com.projeto.sistema_epi.service.EmprestimoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 @Component
@@ -17,6 +22,12 @@ public class EmprestimoController {
 
     private final EmprestimoService emprestimoService;
 
+    private final EmprestimoRepository emprestimoRepository;
+
+    private final ColaboradorRepository colaboradorRepository;
+
+    private final EpiRepository epiRepository;
+
     private final DateTimeFormatter date = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public void retirarEpi(){
@@ -24,32 +35,46 @@ public class EmprestimoController {
 
         System.out.println("Informe o id do colaborador: ");
         empDto.setIdColaborador(sc.nextLong());
-
-        System.out.println("Informe o id do EPI: ");
-        empDto.setIdEpi(sc.nextLong());
-
         sc.nextLine();
 
-        System.out.println("Data de Emprestimo (dd/MM/yyyy): ");
-        LocalDate dataEmprestimo = LocalDate.parse(sc.nextLine(), date);
-        empDto.setDataEmprestimo(dataEmprestimo.atStartOfDay());
+        if (!colaboradorRepository.findById(empDto.getIdColaborador()).isPresent()){
+            System.out.println("Colaborador não encontrado");
+            return;
+        }
+        System.out.println("Informe o id do EPI: ");
+        empDto.setIdEpi(sc.nextLong());
+        sc.nextLine();
 
+        if (!epiRepository.findById(empDto.getIdEpi()).isPresent()){
+            System.out.println("Epi não encontrado");
+            return;
+        }
 
         System.out.println("Data prevista de devolucao (dd/MM/yyyy): ");
         LocalDate dataPrevistaDevolucao = LocalDate.parse(sc.nextLine(),date);
         empDto.setDataPrevistaDevolucao(dataPrevistaDevolucao);
 
-        sc.nextLine();
         System.out.println("Observação:");
         empDto.setObservacao(sc.nextLine());
 
         emprestimoService.retirarEpi(empDto);
+
+        System.out.println("Empréstimo realizado com sucesso!!");
     }
     public void devolverEpi(){
         System.out.println("Informe o id do emprestimo que a ser devolvido");
         Long id = sc.nextLong();
 
+        if (!emprestimoRepository.findById(id).isPresent()){
+            System.out.println("Empréstimo não encontrado");
+            return;
+        }
 
         emprestimoService.devolverEpi(id);
+
+        System.out.println("Devolução realizada com sucesso!!");
+    }
+    public List<EmprestimoResponseDto> listarEmprestimos(){
+        return emprestimoService.listarEmprestimos();
     }
 }
