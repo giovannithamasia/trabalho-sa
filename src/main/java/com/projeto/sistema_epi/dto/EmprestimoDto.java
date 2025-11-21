8c1d20546b84fe17af5a2c55d2e82957b74be737
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-21 16:03:47 -0300

    refactor: remover campos de datas desnecessários do EmprestimoDtorefactor: remover campos de datas desnecessários do EmprestimoDto

--- src/main/java/com/projeto/sistema_epi/dto/EmprestimoDto.java
+++ src/main/java/com/projeto/sistema_epi/dto/EmprestimoDto.java
@@ -1,15 +1,9 @@
 
 package com.projeto.sistema_epi.dto;
 
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,17 +17,8 @@ public class EmprestimoDto {
     private Long idColaborador;
     private Long idEpi;
 
-
-    @NotNull(message = "Data do Emprestimo não pode ser nula")
-    @PastOrPresent(message = "Data do emprestimo não pode ser futura")
-    private LocalDateTime dataEmprestimo;
-
-    @NotNull(message = "Data Prevista pra devolução não pode ser nula")
     private LocalDate dataPrevistaDevolucao;
 
-    @PastOrPresent(message = "Data da devolução não pode ser futura")
-    private LocalDateTime dataDevolucao;
-
-    @Size(max = 255, message = "Observação mão pode ultrapassar 255 caracteres")
+    @Size(max = 255, message = "Observação não pode ultrapassar 255 caracteres")
     private String observacao;
 }
