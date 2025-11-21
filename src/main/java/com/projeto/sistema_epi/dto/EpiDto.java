b188936c54b80683d2474d3f1d7a7b9107d278fa
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2025-11-21 15:49:10 -0300

    refactor: remover campo situacao do EpiDto

--- src/main/java/com/projeto/sistema_epi/dto/EpiDto.java
+++ src/main/java/com/projeto/sistema_epi/dto/EpiDto.java
@@ -1,7 +1,7 @@
 package com.projeto.sistema_epi.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -21,22 +21,19 @@ public class EpiDto {
     private Long idEpi;
 
     @NotBlank(message = "Nome não pode ser nulo")
-    @Size(max = 120, message = "Nome pode ultrapassar 120 caracteres")
+    @Size(max = 120, message = "Nome não pode ultrapassar 120 caracteres")
     private String nome;
 
-    @Size(max = 80, message = "Tipo pode ultrapassar 80 caracteres")
+    @Size(max = 80, message = "Tipo não pode ultrapassar 80 caracteres")
     private String tipo;
 
-    @Size(max = 255, message = "Descricão pode ultrapassar 255 caracteres")
+    @Size(max = 255, message = "Descricão não pode ultrapassar 255 caracteres")
     private String descricao;
 
-    @Size(max = 20, message = "Tamanho pode ultrapassar 20 caracteres")
+    @Size(max = 20, message = "Tamanho não pode ultrapassar 20 caracteres")
     private String tamanho;
 
-    @NotNull(message = "Validade não pode ser nula")
     private LocalDate validade;
 
-    @Size(max = 20, message = "Situacao pode ultrapassar 20 caracteres")
-    private String situacao;
 
 }
